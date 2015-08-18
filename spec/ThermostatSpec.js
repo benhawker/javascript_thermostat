56ef2b2b4d7e627a23b71c42a3421306f15c9085
Ben <ben.c.hawker@gmail.com>
2015-08-18 09:54:40 +0100

    Tuesday am with Leon

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,15 +1,36 @@
-describe('Thermostat', function(){
+describe("Thermostat", function() {
+  var thermostat;
 
-	var t;
+   beforeEach(function() {
+    thermostat = new Thermostat();
+   });
 
-	beforeEach(function() {
-		t = new Thermostat();
-	});
-	
-	describe('Starting State', function(){
+  describe("Starting state", function() {
 
-		it('has a starting default temperature of 20 degrees', function(){
-			expect(t.start).toEqual(20);
-		});
-	});
+    it("has default temperature of 20",function() {
+      expect(thermostat.temperature).toEqual(20);
+    });
+
+    it("starts with power saving on", function() {
+      expect(thermostat.powerSaving).toEqual("on");
+    });
+
+ });
+
+
+  describe("power saving toggle on and off", function() {
+
+    it("can be turned off, when it is on", function() {
+      expect(thermostat.switchOffPowerSave()).toEqual("off")
+    });
+
+    it("can be turned on, whenit is off", function() {
+      thermostat.switchOffPowerSave();
+      expect(thermostat.switchOnPowerSave()).toEqual("on")
+    });
+
+  });
+
 });
+
+ 
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,6 +1,16 @@
-var Thermostat = function(){};
-
-Thermostat.prototype.start = function{
-	degrees = 20
-	return degrees;
+var Thermostat = function() {
+  this.temperature = DEFAULT_TEMP;
+  this.powerSaving = "on";
 };
+
+var DEFAULT_TEMP = 20;
+
+
+Thermostat.prototype.switchOffPowerSave = function() {
+	return this.powerSaving = "off";
+};
+
+Thermostat.prototype.switchOnPowerSave = function() {
+	return this.powerSaving = "on";
+};
+
